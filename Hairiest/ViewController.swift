1878cdf39a86edf258d0bf1ab77b02e6aafdf210
tashi21412 <tashi21412@gmail.com>
2017-07-16 13:20:09 -0700

    Initial Commit

---
+++ Hairiest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hairiest
+//
+//  Created by Lobsang Tashi on 7/16/17.
+//  Copyright © 2017 Lobsang Tashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
